3cab2b6702015d26b44d3cc81759266bc10fda46
Edward <55427391+UnknownPaws@users.noreply.github.com>
2023-09-12 22:50:30 +0100

    First attempt at image smoothening

--- main.py
+++ main.py
@@ -1,29 +1,30 @@
 import zlib
 
 
-# This code is all temporary and is only for testing deflate decompression using zlib
 def main():
     img = open('lions.png', 'rb')
     mod_img = open('processed_lions.png', 'wb')
 
     pre_data = img.read(179)  # All the chunks before the IDAT chunk plus the length and type bytes (8 bytes combined) from the IDAT chunk
     compressed_image_data = img.read(1106450)  # Image data
-
-    # Decompress and recompress just to make sure it will go back to its original state
     decompressed_image_data = decompress_image(compressed_image_data)
-    recompressed_image_data = compress_image(decompressed_image_data)
-
     post_data = img.read(16)  # CRC32 of IDAT chunk plus the IEND chunk
+
+    modified_data = bytearray()
+    for i in range(0, len(decompressed_image_data)-2, 3):
+        r = decompressed_image_data[i]
+        g = decompressed_image_data[i + 1]
+        b = decompressed_image_data[i + 2]
+        average = (r + g + b) // 3  # Compute the average of RGB channels for grayscale value
+        modified_data.extend([average, average, average])
 
     # Write "new" data to a new file
     mod_img.write(pre_data)
-    mod_img.write(recompressed_image_data)
+    mod_img.write(compress_image(modified_data))
     mod_img.write(post_data)
 
     img.close()
     mod_img.close()
-
-    print(decompressed_image_data)
 
 
 def decompress_image(data):
