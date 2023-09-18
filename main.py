7200695360a4b8db237c784b57355ea6397e5a77
Edward <55427391+UnknownPaws@users.noreply.github.com>
2023-09-18 02:51:57 +0100

    last commit before changing to Pillow

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import zlib
 
+
 def main():
     img = open('pixel_art.png', 'rb')
     mod_img = open('processed.png', 'wb')
@@ -12,6 +13,7 @@ def main():
     idat_index = img.tell()
     img.seek(0, 0)
 
+    # PNG Header, IHDR chunk, plus any other chunks between IHDR and IDAT
     for i in range(0, idat_index):
         byte = img.read(1)
         mod_bytearray.extend(byte)
@@ -22,28 +24,44 @@ def main():
     compressed_image_data = img.read(data_length)  # Image data
     decompressed_image_data = decompress_image(compressed_image_data)
     decompressed_length = len(decompressed_image_data)
-    rgb_vals = decompressed_length - decompressed_length % 3
 
     modified_data = bytearray()
-    for i in range(0, rgb_vals, 3):
-        r = decompressed_image_data[i]
-        g = decompressed_image_data[i + 1]
-        b = decompressed_image_data[i + 2]
-        # print(r, g, b)
-        gray = int(0.299*r+0.587*g+0.114*b)  # Compute the average of RGB channels for grayscale value
-        modified_data.extend([r, g, b])
-    # for i in range(0, decompressed_length % 3 + 1, -1):
-    #     modified_data.extend([decompressed_image_data[decompressed_length-1-i]])
+    image = [[]]
+    filtering_method = []
+    for r in range(0, 10):
+        filtering_method[r] = decompressed_image_data[31 * r]
+        for c in range(1, 31):
+            image[r][c-1] = decompressed_image_data[31 * r + c]
+
+    for i in range(0, 10):
+        row
+        if filtering_method[i] == 0:
+            # None
+            for num in range(0, 10):
+
+        elif filtering_method[i] == 1:
+            # Sub
+        elif filtering_method[i] == 2:
+            # Up
+        elif filtering_method[i] == 3:
+            # Average
+        elif filtering_method[i] == 4:
+            # Paeth
+
+    # modified_data = bytearray()
+    # for i in range(0, rgb_vals, 3):
+    #     r = decompressed_image_data[i]
+    #     g = decompressed_image_data[i + 1]
+    #     b = decompressed_image_data[i + 2]
+    #     print(r, g, b)
+    #     gray = int(0.299*r+0.587*g+0.114*b)  # Compute the average of RGB channels for grayscale value
+    #     modified_data.extend([gray, gray, gray])
+
     if decompressed_length % 3 == 1:
         modified_data.extend([decompressed_image_data[decompressed_length-1]])
     elif decompressed_length % 3 == 2:
         modified_data.extend([decompressed_image_data[decompressed_length-2], decompressed_image_data[decompressed_length-1]])
     recompressed_image_data = compress_image(bytes(modified_data))
-
-    # PNG Header, IHDR chunk, plus any other chunks between IHDR and IDAT
-    for i in range(0, idat_index):
-        byte = img.read(1)
-        mod_bytearray.extend(byte)
 
     # IDAT data length and type
     idat_type = [b'I', b'D', b'A', b'T']
@@ -58,7 +76,6 @@ def main():
     # IDAT CRC32 checksum
     crc32_value = zlib.crc32(b'IDAT' + recompressed_image_data, 0)
     crc32_bytes = crc32_value.to_bytes(4, 'big')
-    print(crc32_bytes)
     mod_bytearray.extend(crc32_bytes)
 
     # IEND chunk
